cf0c8ed7022936c6ebf57a62ee4de12ad72d8032
Saransh Raina <saranshraina1@gmail.com>
2023-04-26 14:16:36 -0500

    Testing struct

--- TestAllocator.cpp
+++ TestAllocator.cpp
@@ -1,14 +1,3 @@
-// -----------------
-// TestAllocator.cpp
-// -----------------
-
-// https://github.com/google/googletest
-// https://github.com/google/googletest/blob/master/docs/reference/assertions.md
-
-// --------
-// includes
-// --------
-
 #include <algorithm> // count
 #include <cstddef>   // ptrdiff_t
 #include <string>    // string
@@ -18,3 +7,26 @@
 #include "Allocator.hpp"
 
 using namespace std;
+
+struct A {
+    friend bool operator == (const A&, const A&) {
+        A::log += "==(A, A) ";
+        return true;
+    }
+
+    static string log;
+
+    A  ()         = default;
+    A  (int)      {
+        log += "A(int) ";
+    }
+    A  (const A&) {
+        log += "A(A) ";
+    }
+    ~A ()         {
+        log += "~A() ";
+    }
+};
+
+string A::log;
+
