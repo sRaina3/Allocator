09e068c4964612ec6e847f04df4e4a52e043f542
Saransh Raina <saranshraina1@gmail.com>
2023-04-26 14:15:48 -0500

    Testing started

---
+++ TestAllocator.cpp
@@ -0,0 +1,20 @@
+// -----------------
+// TestAllocator.cpp
+// -----------------
+
+// https://github.com/google/googletest
+// https://github.com/google/googletest/blob/master/docs/reference/assertions.md
+
+// --------
+// includes
+// --------
+
+#include <algorithm> // count
+#include <cstddef>   // ptrdiff_t
+#include <string>    // string
+
+#include "gtest/gtest.h"
+
+#include "Allocator.hpp"
+
+using namespace std;
